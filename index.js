8aa76b373ae05a31c6732328dfd25359f1abbcdf
Wandati <Petersimon801@gmail.com>
2023-06-16 12:15:38 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+let customerName = 'bob';
+const FavoriteCustomer = 'leastFavoriteCustomer';
+
+function FixTheScope(){
+    return customerName
+}
+FixTheScope()
+function upperCaseCustomerName(){
+    return customerName.toUpperCase()
+}
+upperCaseCustomerName()
+function setBestCustomer(){
+    let bestCustomer = 'not bob';
+    return bestCustomer
+    
+}
+setBestCustomer()
+function overwriteBestCustomer(){
+    let newname = setBestCustomer();
+    return newname.replace('maybe bob')
+}
+overwriteBestCustomer()
+
+function changeLeastFavoriteCustomer(){
+     FavoriteCustomer = "Least Favoured customer";
+    return FavoriteCustomer;
+}
+changeLeastFavoriteCustomer()
